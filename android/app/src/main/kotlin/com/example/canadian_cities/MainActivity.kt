3c0b18fa66d84cf21b4a97b316a03ccabebe9d4f
Jiangyu Dai <dai00055@algonquincollege.com>
2025-02-14 13:11:29 -0500

    Canadian_cities

---
+++ android/app/src/main/kotlin/com/example/canadian_cities/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.canadian_cities
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+// CST2335 Week 6 - ListView with ListTile
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CST2335 - Merge Activity',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Week 6/7 - Canadian Cities'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var myCities = <String>[
+    "Toronto",
+    "Montreal",
+    "Vancouver",
+    "Calgary",
+    "Edmonton",
+    "Ottawa",
+    "Winnipeg",
+    "Quebec City",
+    "Hamilton",
+    "Kitchener"
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title)),
+      body: ListView.builder(
+        itemCount: myCities.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(myCities[index]),
+            subtitle: Text("Description ..."),
+            leading: const Icon(Icons.location_on, color: Colors.grey),
+            onTap: () {
+              print(myCities[index]);
+            },
+            trailing: IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                setState(() {
+                  print("Delete ${myCities[index]}");
+                });
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
